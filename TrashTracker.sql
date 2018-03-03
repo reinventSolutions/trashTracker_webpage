90e20f1de7f837588c51fd359feb97c1cd8cf8d9
Kevin Truebe <kevin.truebe@gmail.com>
2018-03-02 22:34:31 -0800

    Add files via upload
    currently implemented database

--- TrashTracker.sql
+++ TrashTracker.sql
@@ -13,10 +13,11 @@ CREATE TABLE users (
   email VARCHAR(200) NOT NULL,
   CONSTRAINT IDPK PRIMARY KEY (ID)
 );
-
 CREATE TABLE Houses (
-  Address VARCHAR(100) NOT NULL,
+  House INTEGER(20) NOT NULL,
   UsernameID INTEGER NOT NULL,
+  Address VARCHAR(100) NOT NULL,
+  St VARCHAR(100) NOT NULL,
   City VARCHAR(100) NOT NULL, 
   Zip INTEGER(7) NOT NULL,
   N1 VARCHAR(8) NOT NULL,
@@ -24,13 +25,13 @@ CREATE TABLE Houses (
   N3 VARCHAR(8) NOT NULL,
   N4 VARCHAR(8) NOT NULL,
   N5 VARCHAR(8) NOT NULL,
-  CONSTRAINT HousePK PRIMARY KEY (Address),
+  CONSTRAINT HousePK PRIMARY KEY (House),
   CONSTRAINT UsernameFK FOREIGN KEY (UsernameID) REFERENCES users(ID)
 );
 
-Create TABLE Weights (
+CREATE TABLE Weights (
   Weight INTEGER(50) NOT NULL,
-  HouseID VARCHAR(20) NOT NULL,
+  HouseID INTEGER(20) NOT NULL,
   Con INTEGER(10) NOT NULL,
   BinType VARCHAR(200) NOT NULL,
   Wk VARCHAR(15) NOT NULL,
@@ -38,7 +39,7 @@ Create TABLE Weights (
   Dte INTEGER(6) NOT NULL,
   Impute VARCHAR(15) NOT NULL,
   CONSTRAINT WeightPK PRIMARY KEY (Weight),
-  CONSTRAINT HouseFK FOREIGN KEY (HouseID) REFERENCES Houses(Address)
+  CONSTRAINT HouseFK FOREIGN KEY (HouseID) REFERENCES Houses(House)
 );
 CREATE TABLE Bins (
  Bin INTEGER (10) NOT NULL AUTO_INCREMENT,
@@ -51,7 +52,6 @@ CREATE TABLE Bins (
  CONSTRAINT BinPK PRIMARY KEY (Bin),
  CONSTRAINT WeightFK FOREIGN KEY (WeightID) REFERENCES Weights(Weight)
 );
-
 CREATE TABLE Routes (
   RouteNumber INTEGER(10) NOT NULL,
   WeightID INTEGER(50) NOT NULL,
@@ -59,7 +59,6 @@ CREATE TABLE Routes (
   CONSTRAINT RouteNumberPK PRIMARY KEY (RouteNumber),
   CONSTRAINT WeightFK2 FOREIGN KEY(WeightID) REFERENCES Weights(Weight)
 );
-
 CREATE TABLE Trucks (
   LicensePlate VARCHAR(10) NOT NULL,
   TruckNumber VARCHAR(10) NOT NULL,
@@ -67,18 +66,17 @@ CREATE TABLE Trucks (
   CONSTRAINT LicensePlatePK PRIMARY KEY (LicensePlate),
   CONSTRAINT RouteFK FOREIGN KEY (RouteNumber) REFERENCES Routes(RouteNumber)
 );
-
 CREATE TABLE Drivers (
- EmployeeID VARCHAR(15) NOT NULL,
- Name VARCHAR(50) NOT NULL,
- TruckID VARCHAR(10) NOT NULL,
- CONSTRAINT EmployeePK PRIMARY KEY (EmployeeID),
- CONSTRAINT TruckFK FOREIGN KEY(TruckID) REFERENCES Trucks(LicensePlate)
+  EmployeeID VARCHAR(15) NOT NULL,
+  Name VARCHAR(50) NOT NULL,
+  TruckID VARCHAR(10) NOT NULL,
+  CONSTRAINT EmployeePK PRIMARY KEY (EmployeeID),
+  CONSTRAINT TruckFK FOREIGN KEY(TruckID) REFERENCES Trucks(LicensePlate)
 );
 
-
-INSERT INTO `users` (`ID`, `password`, `email`, `name`) VALUES
-(12345, '12345', '12345', 'Sample');
+INSERT INTO `users` (`ID`, `password`, `email`) VALUES
+(12345, '12345', '12345');
 INSERT INTO `users` (`ID`, `password`, `email`, `name`) VALUES
 (11111, 'password', 'Scotty@gmail.com', 'Scotty');
-
+INSERT INTO `Houses` (`UsernameID`, `House`, `Address`, `City`, `St`, `Zip`) VALUES
+(11111, 099878, '123 Blah Ln', 'San Marcos', 'CA', '92078');
