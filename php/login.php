291f8c9a580327b4d7a49126924babb55663efa8
d3nise <denise.thuyvy@gmail.com>
2018-03-13 15:56:20 -0700

    added db info

--- php/login.php
+++ php/login.php
@@ -1,3 +1,4 @@
+<?php include "dbinfo.php"; ?>
 <html>
 <html lang="en">
 <head>
@@ -17,50 +18,71 @@
 <body>
 <?php
   
+session_start();
+/*
 $DBservername = "reinvent-solutions-rds-instance-id.ck1gum76iw9m.us-west-2.rds.amazonaws.com";
 $DBusername = "reinvent";
 $DBpassword = "solutions";
 $DBname = "REINVENTSOLUTIONS";
-
- /* Connect to MySQL and select the database. */
+  Connect to MySQL and select the database. */
 //  $connection = mysqli_connect($DBservername, $DBusername, $DBpassword);
-//	Or die("<div class='error' ><p>Could not connect to mysql.<br>Error Code" . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p></div>");
+//  Or die("<div class='error' ><p>Could not connect to mysql.<br>Error Code" . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p></div>");
   
 //  @mysqli_select_db($DBname, $connection)
 //    Or die("<div class='error'><p>Could not connect to database<br>Error Code" . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p></div>");
 //
-	$connection = mysqli_connect($DBservername, $DBusername, $DBpassword);
-	@mysqli_select_db($connection, $DBname);
-	
-//
+  $connection = mysqli_connect($DBservername, $DBusername, $DBpassword);
+  @mysqli_select_db($connection, $DBname);
   
-	   if($connection){
+//
+
+     if($connection){
           $password = $_POST['userPassword'];//input password
           $email = $_POST['userEmail'];//input email
         
-          $userLogin = "SELECT password, email, ID FROM users WHERE email = '$email'";
+          $userLogin = "SELECT password, email, ID, name FROM users WHERE email = '$email'";
           $result = @mysqli_query($connection, $userLogin);
           $row = mysqli_fetch_row($result);
           $pass = $row[0]; //database password
-          $mail = $row[1]; //dataase email
+          $mail = $row[1]; //database email
           $id = $row[2]; //database userID
+          $name = $row[3]; //database name
+
+          $_SESSION["name"] = $name;
+
+          $getAddress = "SELECT UsernameID, Address, St, City, Zip FROM Houses WHERE UsernameID ='$id'";
+
+          $addressResult = @mysqli_query($connection, $getAddress);
+          $row2 = mysqli_fetch_row($addressResult);
+          $userid = $row2[0];
+          $address = $row2[1];
+          $state = $row2[2];
+          $city = $row2[3];
+          $zip = $row2[4];
+
+          $_SESSION["Address"] = $address; 
+          $_SESSION["St"] = $state;
+          $_SESSION["City"] = $city;
+          $_SESSION["Zip"] = $zip;
+
         
-		if($id == $password ){
-              header("Location: http://ec2-54-201-184-63.us-west-2.compute.amazonaws.com/Scotty_Test/php/signup.php");//make changes here
+    if($id == $password ){
+              header("Location: http://ec2-54-201-184-63.us-west-2.compute.amazonaws.com/TrashTracker/php/signup.php");//make changes here
             exit();
-		}
+    }
         else if(($pass == $password && $mail == $email)&&($id !== $pass)){
-              header("Location: http://ec2-54-201-184-63.us-west-2.compute.amazonaws.com/Scotty_Test/php/account.php");//make chages here
+              $_SESSION[logged_in] = true;
+              header("Location: http://ec2-54-201-184-63.us-west-2.compute.amazonaws.com/TrashTracker/php/account.php");//make chages here
             exit();
         }
-		else{ 
-			header("Location: http://ec2-54-201-184-63.us-west-2.compute.amazonaws.com/Scotty_Test/php/index.php");//make changes here
-			exit();
+    else{ 
+      header("Location: http://ec2-54-201-184-63.us-west-2.compute.amazonaws.com/TrashTracker/php/index.php");//make changes here
+      exit();
         } 
         }
       else{ 
-			header("Location: http://ec2-54-201-184-63.us-west-2.compute.amazonaws.com/Scotty_Test/php/index.php");//make changes here
-			exit();
+      header("Location: http://ec2-54-201-184-63.us-west-2.compute.amazonaws.com/TrashTracker/php/index.php");//make changes here
+      exit();
         } 
      
      mysqli_close($connection);
@@ -70,7 +92,7 @@ $DBname = "REINVENTSOLUTIONS";
   <!--
     DENISE THUY VY NGUYEN
     2/1/2018
-    SCOTTY CARDWELL
-    3/2/2018
+  SCOTTY CARDWELL
+  3/2/2018
   --> 
 </html>
