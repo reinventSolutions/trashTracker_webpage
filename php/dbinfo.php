0df27a3dc13a6024384c0ce386d180c060c96f61
d3nise <denise.thuyvy@gmail.com>
2018-03-28 04:12:26 -0700

    db info

--- php/dbinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$DBservername = "reinvent-solutions-rds-instance-id.ck1gum76iw9m.us-west-2.rds.amazonaws.com";
-$DBusername = "reinvent";
-$DBpassword = "solutions";
-$DBname = "REINVENTSOLUTIONS";
-
-?>
