cf4ff7e238bb8bc8d98f65f2ca132ef59cd732fc
d3nise <denise.thuyvy@gmail.com>
2018-04-15 16:20:56 -0700

    old P2 page reloads when pressing next/prev

--- php/account1.php
+++ php/account1.php
@@ -1,8 +1,5 @@
 <?php include "../../DB/dbinfo.php"; ?>
-
-<?php
-  session_start();
-?>
+<?php session_start();?>
 
 <?php
   session_start();
@@ -13,102 +10,103 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Trash Tracker</title>
-        <link rel="icon" href="../images/trashtracker.png"/>
-        <!--BOOTSTRAP-->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <!--CSS-->
-        <link rel="stylesheet" href="../css/stylesheet.css">
-        <!--CSS MEDIA QUERY-->
-        <link rel="stylesheet" href="../css/stylesheet2.css">
-        <!-- ICONS https://material.io/icons/#ic_cloud-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--POPOVER
-        https://www.w3schools.com/bootstrap4/bootstrap_popover.asp
-        -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-        <script type="text/javascript" src="../js/popover.js"></script>
-        <!--GRAPH-->
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <script type="text/javascript" src="../js/nextWeek.js"></script>
-        <script type="text/javascript" src="../js/prevWeek.js"></script>
-        <!--<script type="text/javascript" src="../js/graph.js"></script>-->          
-        
-        <!--PHP pulls data into graph
-        https://stackoverflow.com/questions/39658251/how-to-use-php-echo-data-in-order-to-populate-a-google-chart
-        -->
-        <?php include "graph.php"; ?>
-        <?php include "comparison.php"; ?>
-        <!--
-          PHP to pull data from graphs
-        -->
-        <script type="text/javascript">
-          /* https://jsfiddle.net/2f3kLtzq/5/
-          */
-          google.charts.load('current', {'packages':['bar']});
-          google.charts.setOnLoadCallback(drawChart);
-
-          function drawChart() {
-            <?php echo $data; ?>
-        
-            var options = {
-              chart: {
-                title: 'Trash Tracker',
-                subtitle: 'Weekly Trash View',
-              },
-              axes: {
-                  y: {
-                      all: {
-                          range: {
-                              y: 100,
-                              y:75,
-                              y:50,
-                              y:25,
-                              y: 0
-                          }
-                      }
-                  }
-              },
-              bars: 'vertical',
-              vAxis: {
-                  title: 'Total weight in pounds',
-                  format: 'decimal',
-                  minValue: 0,
-                },
-             colors: ['#0066ff', '#808080', '#7aac3b']
-            };
-
-            var chart = new google.charts.Bar(document.getElementById('chart_div'));
-
-            chart.draw(data, google.charts.Bar.convertOptions(options));
-
-            var btns = document.getElementById('btn-group');
-
-            btns.onclick = function (e) {
-
-              if (e.target.tagName === 'BUTTON') {
-                options.vAxis.format = e.target.id === 'none' ? '' : e.target.id;
-                chart.draw(data, google.charts.Bar.convertOptions(options));
-              }
-            }
-          }
-        </script>
-        
-        <script type ="text/javascript">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Trash Tracker</title>
+    <link rel="icon" href="../images/trashtracker.png"/>
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <!--CSS-->
+    <link rel="stylesheet" href="../css/stylesheet.css">
+    <!--CSS MEDIA QUERY-->
+    <link rel="stylesheet" href="../css/stylesheet2.css">
+    <!-- ICONS https://material.io/icons/#ic_cloud-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--GENERAL BIN INFO-->
+    <script type="text/javascript" src="../js/popover.js"></script>
+    <!--GRAPH-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <!-- BUTTON FUNCTIONS FOR GOOGLE.CHARTS -->
+	  <script type="text/javascript">
+	  $(document).ready(function(){
+      var lower = 0;
+      var upper = 5;
+      $("#weeklyview").click(function(){
+      $("#chart_div").show();//Weekly View
+      $("#weeklybuttons").show();
+      $("#chart_div2").hide();//Monthly View
+      $("#monthlybuttons").hide();
+      $("#chart_div3").hide();//Yearly View
+      $("#yearlybuttons").hide();
+    });
+    $("#nextweek").click(function(e){
+		  $("#DData").empty();
+          $("#chart_div").empty();
+      lower = lower +1;
+      upper = upper +1;
+      if(upper > 9){
+        lower = 0;
+        upper = 5;
+      }
+        $.post("graph2.php", {
+          lowerValue: lower,
+          upperValue: upper
+        }, function(data, status){
+          $("#DData").html(data, status);
+        });
+      });
+      $("#prevweek").click(function(e){
+      lower = lower -1;
+      upper = upper -1;
+      if (lower < 0){
+        lower = 4;
+        upper = 9;
+      }
+        $.post("graph2.php", {
+          lowerValue: lower,
+          upperValue: upper
+        }, function(data, status){
+          $("#DData").empty();
+          $("#chart_div").empty();
+          $("#DData").html(data, status);
+        });
+      });
+      $("#monthlyview").click(function(){
+      $("#chart_div").hide();//Weekly View
+      $("#weeklybuttons").hide();//hide buttons div
+      $("#chart_div2").show();//Monthly View
+      $("#monthlybuttons").show();
+      $("#chart_div3").hide();//Yearly View
+      $("#yearlybuttons").hide();
+      
+        $.post("monthlyGraph.php", {
+          lowerValue: lower,
+          upperValue: upper
+        }, function(data, status){
+          $("#DData").html(data, status);
+        });
+      });
+    })
+	</script>
+		<script type="text/javascript" src="../js/nchouse.js"></script>	
+		<!--FUNCTIONS FOR IMAGE HOUSE FLIPS/THUMBS STACK-->
+		<!-- <script type="text/javascript" src="../js/ratio.js"></script> -->
+		<?php include "graph.php"; ?>
+		<?php include "comparison.php"; ?>
+		<script type = "text/javascript">
         //GENERAL INFO YOUR RATIO 
+        ratioInfo();
         function ratioInfo(){
-          var owner = <?php echo $housecompare ?>;
-          var neighbor = <?php echo $neighborcomparison?>;
-          var elem = document.createElement("img")
-
-          document.getElementById("yourratiobig").appendChild(elem).style.width = "50%";
-          if(owner < neighbor)
+        var owner = "<?php echo $housecompare ?>";
+        var neighbor = "<?php echo $neighborcomparison?>";
+        var elem = document.createElement("img")
+        document.getElementById("yourratiobig").appendChild(elem).style.width = "50%";
+        if(owner < neighbor)
           elem.src = '../images/orangehouse.png';
-          else
-          elem.src = '../images/greenhouse.png';
+        else
+        elem.src = '../images/greenhouse.png';
         }
         //THUMBS FUNCTION
         function thumbs(){
@@ -117,24 +115,24 @@
           var elem = document.createElement("img")
           document.getElementById("thumbupdown").appendChild(elem).style.width = "50%";
         if(neighbor < owner)
-        elem.src = '../images/thumb.png';
+         elem.src = '../images/thumb.png';
         else
-        elem.src = '../images/thumbDown.png';
+         elem.src = '../images/thumbDown.png';
         }
         //MAIN RATIO 
         function mainRatio(){
           var owner = <?php echo $housecompare ?>;
           var neighbor = <?php echo $neighborcomparison?>;
           var elem = document.createElement("img")
-          document.getElementById("yourratio").appendChild(elem).style.width = "15%";
-          document.getElementById("yourratio").style.aligncontent = "center";
+          document.getElementById("mr").appendChild(elem).style.width = "15%";
+          document.getElementById("mr").style.aligncontent = "center";
         if(owner < neighbor)
           elem.src = '../images/orangehouse.png';
 
         else
           elem.src = '../images/greenhouse.png';
         }
-        //NORMAL HOUSE AVERAGE ONE FOR EACH HOUSE
+        switchImageN1();
         function switchImageN1(){
           var owner = <?php echo $housecompare ?>; 
           var neighbor = <?php echo $neighborcomparison?>;
@@ -145,9 +143,10 @@
         else
           elem.src = '../images/greenhouse.png';
         }
+        switchImageN2();
         function switchImageN2(){
-          var owner = <?php echo $housecompare ?>; 
-          var neighbor = <?php echo $neighborcomparison?>;
+          var owner = "<?php echo $housecompare ?>"; 
+          var neighbor = "<?php echo $neighborcomparison?>";
           var elem = document.createElement("img")
           document.getElementById("orangegreenhouse2").appendChild(elem).style.width = "25%";
         if(neighbor < owner)
@@ -155,9 +154,10 @@
         else
           elem.src = '../images/greenhouse.png';
         }
+        switchImageN3();
         function switchImageN3(){
-          var owner = <?php echo $housecompare ?>; 
-          var neighbor = <?php echo $neighborcomparison?>;
+          var owner = "<?php echo $housecompare ?>"; 
+          var neighbor = "<?php echo $neighborcomparison?>";
           var elem = document.createElement("img")
           document.getElementById("orangegreenhouse3").appendChild(elem).style.width = "25%";
         if(neighbor < owner)
@@ -165,9 +165,10 @@
         else
           elem.src = '../images/greenhouse.png';
         }
+        switchImageN4();
         function switchImageN4(){
-          var owner = <?php echo $housecompare ?>; 
-          var neighbor = <?php echo $neighborcomparison?>;
+          var owner = "<?php echo $housecompare ?>"; 
+          var neighbor = "<?php echo $neighborcomparison?>";
           var elem = document.createElement("img")
           document.getElementById("orangegreenhouse4").appendChild(elem).style.width = "25%";
         if(neighbor < owner)
@@ -175,9 +176,10 @@
         else
           elem.src = '../images/greenhouse.png';
         }
+        switchImageN5();
         function switchImageN5(){
-          var owner = <?php echo $housecompare ?>; 
-          var neighbor = <?php echo $neighborcomparison?>;
+          var owner = "<?php echo $housecompare ?>"; 
+          var neighbor = "<?php echo $neighborcomparison?>";
           var elem = document.createElement("img")
           document.getElementById("orangegreenhouse5").appendChild(elem).style.width = "25%";
         if(neighbor < owner)
@@ -185,8 +187,12 @@
         else
           elem.src = '../images/greenhouse.png';
         }
-</script>
+		</script>
+		       
 </head>
+		<div id="DData">
+
+		</div> 
 
 <body><!--START OF BODY-->     
   <header style=""> <!--START OF HEADER-->
@@ -202,9 +208,11 @@
    <div class="row">
    <!--Historical Comparison-->
     <?php include "temps/historicalComp.php"; ?>
+
+
    <div class="row">
    <!--Normal Comparison-->
-    <?php include "temps/normalComp.php"; ?>
+   <?php include "closest.php"; ?>
 
 <!--RECYCLE GAME-->
 <?php include "temps/game.php"; ?>
@@ -218,8 +226,14 @@
 </footer><!--.FOOTER-->
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
-          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <!--<script type="text/javascript" src="../js/onload.js"></script>-->
+  <script>
+  $("#monthlybuttons").hide();
+  $("#yearlybuttons").hide();
+  $("#chart_div2").hide();
+  $("#chart_div3").hide();
+  $("#DData").load("graph1.php");
+  </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" 
           integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
